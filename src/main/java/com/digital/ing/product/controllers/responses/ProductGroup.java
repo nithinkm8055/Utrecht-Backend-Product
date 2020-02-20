6364c16f6d9b1f31a8352ca660927e53213bba4b
Aman Chhabra <amanchhabra1990@gmail.com>
2020-02-20 14:09:23 +0100

    Adding basic rest service

---
+++ src/main/java/com/digital/ing/product/controllers/responses/ProductGroup.java
@@ -0,0 +1,11 @@
+package com.digital.ing.product.controllers.responses;
+
+import java.util.List;
+
+public class ProductGroup {
+    public String id;
+    public String name;
+    public List<Product> products;
+
+
+}
---
+++ src/main/java/com/digital/ing/product/controllers/responses/ProductGroupResponse.java
@@ -0,0 +1,8 @@
+package com.digital.ing.product.controllers.responses;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class ProductGroupResponse {
+    public List<ProductGroup> productGroups;
+}
