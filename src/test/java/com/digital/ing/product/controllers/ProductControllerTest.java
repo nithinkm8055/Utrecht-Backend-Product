ba9a650958e359ca430f64fc4c3a486d79c16ff6
Aman Chhabra <amanchhabra1990@gmail.com>
2020-02-20 16:06:49 +0100

    Adding Junit for controller

--- src/test/java/com/digital/ing/product/controllers/ProductControllerTest.java
+++ src/test/java/com/digital/ing/product/controllers/ProductControllerTest.java
@@ -1,8 +1,31 @@
 package com.digital.ing.product.controllers;
 
+import com.digital.ing.product.controllers.responses.ProductGroupResponse;
+import com.digital.ing.product.services.ProductsService;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(JUnit4.class)
+import static org.mockito.Mockito.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
 public class ProductControllerTest {
+
+    @Mock
+    ProductsService productsService;
+
+    @InjectMocks
+    ProductsController productsController;
+
+    @Test
+    public void getAllProductGroups_whenCalled_shouldCallService(){
+        when(productsService.getAllProducts()).thenReturn(new ProductGroupResponse());
+
+        productsController.getAllProductGroups();
+
+        verify(productsService, times(1)).getAllProducts();
+    }
 }
