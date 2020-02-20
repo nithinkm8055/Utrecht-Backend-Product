3aaee91d82ccb137a473f38d2f7c2e3924cc2d45
chandramohanV <chandru0011@gmail.com>
2020-02-20 14:46:05 +0100

    Insert DB script for Product and Product group

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+insert into ProductGroup values(1,'Mortgage');
+insert into ProductGroup values(2,'Payments');
+insert into ProductGroup values(3,'Saving');
+
+insert into Product values(1,'Bank saving mortgage',1);
+insert into Product values(2,'Interest-only',1);
+insert into Product values(3,'Liner',1);
+
+insert into Product values(4,'PaymentAccount',2);
+insert into Product values(5,'Debit Card',2);
+insert into Product values(6,'Quarterly Limit',2);
+
+insert into Product values(7,'Orange saving account',3);
+insert into Product values(8,'Child saving account',3);
+insert into Product values(9,'Saving deposite',3);
